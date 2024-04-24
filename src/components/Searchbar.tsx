fcbf13875972be17814e5b2e8dc647ede48552c6
Priyanka Singh <priyankasingh8885@gmail.com>
2024-04-24 13:03:19 +0530

    Search Functionality

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,31 +1,49 @@
-import { Box, InputAdornment, InputBase, Paper } from '@mui/material'
-import React from 'react'
+import { Avatar, Box, InputAdornment, InputBase, Paper } from '@mui/material'
+import React, { useState } from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import {db} from '../firebase'
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, } from "firebase/firestore";
+import {Typography} from '@mui/material';
+import '../index.css'
+
 
 function Searchbar() {
   const [username , setUsername] = useState("");
-  const [user, setUser]= useState("");
-  const [err, setErr]= useState("");
+  const [user, setUser]= useState('');
+  const [err, setErr]= useState(false);
   
 
-  const handleSearch= async()=>{
-    const q = query(collection(db, "users"),where("displayName ", "==" ,"username"));
+  const handleSearch= async(event)=>{
 
-    try {
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-     setUser(doc.data())
-  });
-    } catch (error) {
-      setErr(true);
-    }
+    event.preventDefault();
+    console.log("username",username)
+    const q = query(collection(db, "users"),where("displayName", "==", username));
    
+
+  try {
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot.empty) {
+      // Handle case where no users are found with the provided username
+      console.log("No users found with the provided username");
+    } else {
+      // Assuming only one user is expected, get the first document
+      const userDoc = querySnapshot.docs[0];
+      const userData = userDoc.data();
+      setUser(userData);
+    }
+  } catch (error) {
+    // Handle error
+    console.error("Error searching for users:", error);
+    setErr(true);
   }
+};
+
+  
+  
 
   const handleKey = (e) =>{
-    e.code==="Enter"&& handleSearch();
+   
+    e.code==="Enter"&& handleSearch(e);
 
   }
 
@@ -62,6 +80,12 @@ function Searchbar() {
               }
             />
           </Paper>
+              {err && <span>User not found!</span>}
+              
+              {user && <Box  className='user' sx={{display:'flex',alignItems:'center', gap:2,cursor:'pointer', p:2, mr:3,border:'2px solid red'}}>
+                <Avatar alt={user.displayName} src={user.photoURL} sx={{borderRadius:'50%', height:'4.5rem', width:'4.5rem'}}/>
+                <Typography variant='h6' sx={{color:'#fff'}}> {user.displayName}</Typography>
+              </Box>}
         </Box>
   )
 }
